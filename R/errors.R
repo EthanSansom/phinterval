1390063faf298f8578c1a3f1ae79190ec82ef984
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2024-02-10 12:35:40 -0500

    Add error constructors for argument checks

--- R/errors.R
+++ R/errors.R
@@ -1,3 +1,98 @@
-stop_wrong_type <- function(...) {
+# TODO:
+# - think about nicely designed and re-usable error messages with `cli`. Good for consistency.
+#   - Notes on error constructors: https://design.tidyverse.org/err-constructor.html
+#   - Notes on error messages: https://style.tidyverse.org/error-messages.html
+
+# TODO Ethan: Clean this up to handle the null v. non-null `n` paths
+stop_wrong_class <- function(
+    x,
+    cls,
+    n = NULL,
+    arg = rlang::caller_arg(x),
+    error_call = rlang::caller_env()
+  ) {
+
+  if (is.null(n)) {
+
+    if (!inherits(x, cls)) {
+      cli::cli_abort(
+        paste0(
+          "{.arg {arg}} must be a {.cls cls} vector, ",
+          "not a {.cls {class(x)}}."
+        ),
+        arg = arg,
+        call = error_call
+      )
+    }
+
+  } else {
+
+    if (!inherits(x, cls)) {
+      len_msg <- if (n == 1) "scalar" else paste0("length-", n)
+      cli::cli_abort(
+        paste0(
+          "{.arg {arg}} must be a {len_msg} {.cls cls} vector, ",
+          "not a {.cls {class(x)}}."
+        ),
+        arg = arg,
+        call = error_call
+      )
+    } else if (length(x) != n) {
+      cli::cli_abort(
+        c(
+          "{.arg {arg}} must have length {n}.",
+          i = "{.arg {arg}} has length {length(x)}."
+        ),
+        arg = arg,
+        call = error_call
+      )
+    }
+
+  }
+
+}
+
+# TODO Ethan: Not sure if I need this broad of a function... not sure
+# rlang::caller_arg pattern is great: https://rlang.r-lib.org/reference/caller_arg.html
+stop_arg_not <- function(
+    x,
+    predicate,
+    must_be,
+    not,
+    arg = rlang::caller_arg(x),
+    error_call = rlang::caller_env()
+  ) {
+
+  if (!predicate(x)) {
+    cli::cli_abort(
+      paste0("{.arg {arg}} must be ", must_be, ", not ", not, "."),
+      arg = arg,
+      call = error_call
+    )
+  }
+
+}
+
+check_is_phinty <- function(
+    x,
+    arg = rlang::caller_arg(x),
+    error_call = rlang::caller_env()
+  ) {
+
+  if (is_phinterval(x)) {
+    return(x)
+  }
+  if (lubridate::is.interval(x)) {
+    return(as_phinterval(x))
+  }
+
+  cli::cli_abort(
+    paste0(
+      "{.arg {arg}} must be a {.cls phinterval} or a {.cls Interval} vector, ",
+      "not a {.cls {class(x)}}."
+    ),
+    arg = arg,
+    call = error_call
+  )
 
 }
