b696f7de477a6a196c655914a8a91110eec68626
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2024-02-20 16:32:47 -0500

    Implement `recycle2_common(x, y)` to recycle `x` and `y` vectors to a common size with `base`-ish recycling rules.

--- R/utils.R
+++ R/utils.R
@@ -5,6 +5,39 @@ pmap_lgl <- function(.l, .f, ...) {
   as.vector(pmap(.l, .f, ...), "logical")
 }
 
+# Implements common `base` recycling rules (failing if impossible). See
+# https://vctrs.r-lib.org/articles/type-size.html#appendix-recycling-in-base-r
+# for `base` recycling rules vs. `vctrs` recycling rules.
+recycle2_common <- function(
+    x,
+    y,
+    x_arg = rlang::caller_arg(x),
+    y_arg = rlang::caller_arg(y),
+    error_call = rlang::caller_env()
+  ) {
+
+  x_len <- length(x)
+  y_len <- length(y)
+
+  if (x_len == y_len) {
+    list(x = x, y = y)
+  } else if (x_len == 1) {
+    list(x = vctrs::vec_rep(x, y_len), y = y)
+  } else if (y_len == 1) {
+    list(x = x, y = vctrs::vec_rep(y, y_len))
+  } else if (isTRUE(x_len %% y_len == 0)) {
+    list(x = x, y = vctrs::vec_rep(y, x_len / y_len))
+  } else if (isTRUE(y_len %% x_len == 0)) {
+    list(x = vctrs::vec_rep(x, y_len / x_len), y = y)
+  } else {
+    cli::cli_abort(
+      "Can't recycle {.arg {x_arg}} (size {x_len}) to match {.arg {y_arg}} (size {y_len}).",
+      call = error_call
+    )
+  }
+
+}
+
 # predicates -------------------------------------------------------------------
 
 # TODO Ethan: I don't like any of these predicates, find and replace them.
