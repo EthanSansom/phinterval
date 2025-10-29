1f5e04d2421f10b4e41dea1a92e0c4e27df40212
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2025-10-29 16:30:14 -0400

    Implement a faster squash for lubridate::Interval

--- src/interval.cpp
+++ src/interval.cpp
@@ -1,8 +1,48 @@
+#include "squash.h"
+#include "endpoint.h"
+#include "utils.h"
 #include <Rcpp.h>
 using namespace Rcpp;
 
 // [[Rcpp::export]]
-List interval_to_interval_sets(const DatetimeVector starts, const NumericVector spans) {
+NumericMatrix cpp_squash_lubridate_interval(
+    const DatetimeVector& starts,
+    const NumericVector& spans,
+    bool na_rm
+) {
+  int n = starts.size();
+
+  Endpoints endpoints;
+  endpoints.reserve(n * 2);
+  double start;
+  double span;
+  for (int i { 0 }; i < n; ++i) {
+    start = starts[i];
+    span = spans[i];
+    if (na_rm && (ISNAN(start) || ISNAN(span))) continue;
+    if (!na_rm && (ISNAN(start) || ISNAN(span))) return na_interval();
+
+    if (span < 0) {
+      endpoints.push_back(Endpoint { true, start + span });
+      endpoints.push_back(Endpoint { false, start });
+    } else {
+      endpoints.push_back(Endpoint { true, start });
+      endpoints.push_back(Endpoint { false, start + span });
+    }
+  }
+
+  // Case where `na_rm` and we've only encountered NA values
+  if (endpoints.empty()) return na_interval();
+
+  std::sort(endpoints.begin(), endpoints.end());
+  return squash(endpoints);
+}
+
+// [[Rcpp::export]]
+List lubridate_interval_to_interval_sets(
+    const DatetimeVector& starts,
+    const NumericVector& spans
+  ) {
   int n = spans.size();
   List out(n);
 
