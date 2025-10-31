451baec490e06656f9b7d7ea1fb370cba23a4b89
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2025-10-31 11:37:29 -0400

    Use standalone type-checks for assertions, use rlang as an @import, and remove unneeded imports

--- R/phinterval-package.R
+++ R/phinterval-package.R
@@ -3,17 +3,8 @@
 
 ## usethis namespace: start
 #' @import vctrs
-#' @importFrom lubridate %within% NA_POSIXct_
-#' @importFrom lubridate as.duration
-#' @importFrom lubridate as.interval
-#' @importFrom lubridate duration
-#' @importFrom lubridate interval
-#' @importFrom lubridate is.interval
-#' @importFrom lubridate tz
-#' @importFrom methods setOldClass
+#' @import rlang
 #' @importFrom Rcpp sourceCpp
-#' @importFrom rlang %||%
-#' @importFrom stats setNames
 #' @useDynLib phinterval, .registration = TRUE
 ## usethis namespace: end
 NULL
