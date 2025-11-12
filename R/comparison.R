3441d15eb5ad5aa1163c53f8c4d1d3b1eea3f76c
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2025-11-12 13:12:35 -0500

    Implement comparison operators <, <=, >, >=

--- R/comparison.R
+++ R/comparison.R
@@ -7,6 +7,33 @@ vec_proxy_equal.phinterval <- function(x, ...) {
 # - Yes see bit64 implementation of the individual comparison operators:
 #   https://github.com/r-lib/bit64/blob/main/R/integer64.R
 #' @export
-vec_proxy_compare.phinterval <- function(x, ...) {
-  stop("vec_proxy_compare is not implemented")
+`<.phinterval` <- function(e1, e2) {
+  check_is_phintish(e1)
+  check_is_phintish(e2)
+  check_recycleable(e1, e2)
+
+  cpp_interval_sets_lt(vec_data(as_phinterval(e1)), vec_data(as_phinterval(e2)))
+}
+
+#' @export
+`>.phinterval` <- function(e1, e2) {
+  e2 < e1
+}
+
+#' @export
+`<=.phinterval` <- function(e1, e2) {
+  check_is_phintish(e1)
+  check_is_phintish(e2)
+  check_recycleable(e1, e2)
+
+  cpp_interval_sets_leq(vec_data(as_phinterval(e1)), vec_data(as_phinterval(e2)))
+}
+
+#' @export
+`>=.phinterval` <- function(e1, e2) {
+  check_is_phintish(e1)
+  check_is_phintish(e2)
+  check_recycleable(e1, e2)
+
+  cpp_interval_sets_geq(vec_data(as_phinterval(e1)), vec_data(as_phinterval(e2)))
 }
