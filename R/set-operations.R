cf17cf360daaaaa0839bcca7122e166015ca7133
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2025-11-12 13:16:29 -0500

    Add C++ backends to phinterval set operations

--- R/set-operations.R
+++ R/set-operations.R
@@ -1,100 +1,142 @@
+# todos ------------------------------------------------------------------------
+
+# TODO:
+# - Recycling for pairwise operations!
+
+# squash -----------------------------------------------------------------------
+
 #' @export
 phint_squash <- function(phint, na.rm = TRUE) {
   UseMethod("phint_squash")
 }
 
 #' @export
+phint_squash.default <- function(phint, na.rm = TRUE) {
+  check_is_phintish(phint)
+}
+
+#' @export
 phint_squash.phinterval <- function(phint, na.rm = TRUE) {
-  stop_wrong_class(na.rm, "logical", n = 1L)
-
-  if (!na.rm && anyNA(phint)) {
-    return(na_phinterval(tzone = get_tzone(phint)))
-  }
-
-  # <phinterval> NA values are represented as NULL elements in the underlying
-  # list vector. rbind() here removes all NULL elements and returns a matrix,
-  # except for when every element is NULL, in which case NULL is returned.
-  interval_set <- do.call(rbind, vec_data(phint))
-  if (is.null(interval_set)) {
-    return(na_phinterval(tzone = get_tzone(phint)))
-  }
-
+  check_bool(na.rm)
   new_phinterval(
-    interval_sets = cpp_squash_interval_set(interval_set),
+    interval_sets = phint_to_interval_set(phint, na.rm = na.rm),
     tzone = attr(phint, "tzone")
   )
 }
 
 #' @export
 phint_squash.Interval <- function(phint, na.rm = TRUE) {
-  stop_wrong_class(na.rm, "logical", n = 1L)
+  check_bool(na.rm)
   new_phinterval(
-    interval_sets = int_squash(phint),
+    interval_sets = int_to_interval_set(phint, na.rm = na.rm),
     tzone = attr(phint, "tzone")
   )
 }
 
-int_squash <- function(int, na.rm = TRUE) {
-  starts <- lubridate::int_start(int)
-  spans <- lubridate::int_length(int)
-  cpp_squash_lubridate_interval(starts, spans, na_rm = na.rm)
+phint_to_interval_set <- function(phint, na.rm = TRUE) {
+  if (!na.rm && anyNA(phint)) {
+    return(NULL)
+  }
+  # <phinterval> NA values are represented as NULL elements in the underlying
+  # list vector. rbind() here removes all NULL elements and returns a matrix,
+  # except for when every element is NULL, in which case NULL is returned.
+  interval_set <- do.call(rbind, vec_data(phint))
+  if (is.null(interval_set)) {
+    return(NULL)
+  }
+  cpp_squash_interval_set(interval_set)
 }
+
+int_to_interval_set <- function(int, na.rm = TRUE) {
+  cpp_squash_lubridate_interval(
+    starts = lubridate::int_start(int),
+    spans = lubridate::int_length(int),
+    na_rm = na.rm
+  )
+}
+
+# overlaps ---------------------------------------------------------------------
 
 #' @export
-phint_overlaps <- function(phint1, phint2, inclusive = FALSE) {
+phint_overlaps <- function(phint1, phint2) {
+  check_is_phintish(phint1)
+  check_is_phintish(phint2)
+  check_recycleable(phint1, phint2)
 
-  objs <- recycle2_common(phint1, phint2)
-  phint1 <- check_is_phinty(objs$x)
-  phint2 <- check_is_phinty(objs$y)
-
-  range1 <- rangify_phinterval(phint1)
-  range2 <- rangify_phinterval(phint2)
-
-  non_na_at <- !(is.na(phint1) | is.na(phint2))
-
-  range1_starts <- range1$starts[non_na_at]
-  range1_ends <- range1$ends[non_na_at]
-  range2_starts <- range2$starts[non_na_at]
-  range2_ends <- range2$ends[non_na_at]
-
-  out <- rep(NA, length(phint1))
-  out[non_na_at] <- pmap_lgl(
-    list(
-      x_starts = range1_starts,
-      x_ends = range1_ends,
-      y_starts = range2_starts,
-      y_ends = range2_ends
+  new_phinterval(
+    cpp_interval_sets_overlaps(
+      vec_data(as_phinterval(phint1)),
+      vec_data(as_phinterval(phint2))
     ),
-    range_intersects,
-    inclusive = inclusive
+    tzone = tz_union(phint1, phint2)
   )
-  out
 }
+
+# union ------------------------------------------------------------------------
 
 #' @export
 phint_union <- function(phint1, phint2) {
-  combine_phintervals(
-    .phint1 = phint1,
-    .phint2 = phint2,
-    .f = range_union
+  check_is_phintish(phint1)
+  check_is_phintish(phint2)
+  check_recycleable(phint1, phint2)
+
+  new_phinterval(
+    cpp_union_interval_sets(
+      vec_data(as_phinterval(phint1)),
+      vec_data(as_phinterval(phint2))
+    ),
+    tzone = tz_union(phint1, phint2)
   )
 }
 
+# compliment -------------------------------------------------------------------
+
 #' @export
-phint_intersect <- function(phint1, phint2, inclusive = FALSE) {
-  combine_phintervals(
-    .phint1 = phint1,
-    .phint2 = phint2,
-    .f = range_intersect,
-    inclusive = inclusive
+phint_compliment <- function(phint) {
+  check_is_phintish(phint)
+
+  new_phinterval(
+    cpp_compliment_interval_sets(vec_data(as_phinterval(phint))),
+    tzone = get_tzone(phint)
   )
 }
 
+# intersection -----------------------------------------------------------------
+
 #' @export
-phint_diff <- function(phint1, phint2) {
-  combine_phintervals(
-    .phint1 = phint1,
-    .phint2 = phint2,
-    .f = range_setdifference
+phint_intersect <- function(phint1, phint2) {
+  check_is_phintish(phint1)
+  check_is_phintish(phint2)
+  check_recycleable(phint1, phint2)
+
+  new_phinterval(
+    cpp_intersect_interval_sets(
+      vec_data(as_phinterval(phint1)),
+      vec_data(as_phinterval(phint2))
+    ),
+    tzone = tz_union(phint1, phint2)
   )
+}
+
+# difference -------------------------------------------------------------------
+
+#' @export
+phint_setdiff <- function(phint1, phint2) {
+  check_is_phintish(phint1)
+  check_is_phintish(phint2)
+  check_recycleable(phint1, phint2)
+
+  interval_sets1 <- vec_data(as_phinterval(phint1))
+  interval_sets2 <- vec_data(as_phinterval(phint2))
+  interval_sets <- cpp_intersect_interval_sets(
+    interval_sets1,
+    cpp_compliment_interval_sets(interval_sets2)
+  )
+
+  # A second pass is required to resolve edge-cases where instants aren't
+  # considered to be within intervals that they abut.
+  is_within <- cpp_interval_sets_within(interval_sets2, interval_sets1)
+  interval_sets[is_within] <- list(matrix(numeric(), ncol = 2L))
+
+  new_phinterval(interval_sets, tzone = tz_union(phint1, phint2))
 }
