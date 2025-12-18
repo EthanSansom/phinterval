8531a32e22ab7b02e3c6b48606896bf9966ca0ce
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2025-12-17 19:01:34 -0500

    Add documentation

---
+++ R/params.R
@@ -0,0 +1,24 @@
+#' Parameter Descriptions for phinterval Package
+#'
+#' Default parameter descriptions which may be overridden in individual
+#' functions.
+#'
+#' @param phint1,phint2 `[phinterval / Interval]`
+#'
+#' A pair of `<phinterval>` or `<Interval>` vectors of the same length or of
+#' length 1. Inputs of length 1 are recycled.
+#'
+#' @param phint `[phinterval / Interval]`
+#'
+#' A `<phinterval>` or `<Interval>` vector.
+#'
+#' @return
+#'
+#' The value `NULL`.
+#'
+#' @keywords internal
+#' @aliases parameters arguments args
+#' @usage NULL
+# nocov start
+params <- function(...) NULL
+# nocov end
