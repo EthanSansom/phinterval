f6d6ca0ac99819aaa44940d9325c40bd3d410899
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2025-12-17 19:01:19 -0500

    Add documentation

--- R/phinterval.R
+++ R/phinterval.R
@@ -2,6 +2,53 @@
 
 methods::setOldClass(c("phinterval", "list", "vctrs_vctr"))
 
+#' Create a new phinterval
+#'
+#' @description
+#'
+#' `phinterval()` creates a new `<phinterval>` vector from a list of
+#' [lubridate::interval()] vectors. A phinterval (think "potentially holey interval")
+#' is a span of time which may contain gaps.
+#'
+#' @details
+#'
+#' The `<phinterval>` class is designed as a generalization of the
+#' [lubridate::Interval-class]. While an `<Interval>` element represents a
+#' single contiguous span between two fixed times, a `<phinterval>` element can
+#' represent a time span that may be empty, contiguous, or disjoint (i.e. containing
+#' gaps). Each element of a `<phinterval>` is stored as a (possibly empty) set of
+#' non-overlapping and non-abutting time spans.
+#'
+#' @param intervals `[list of Interval]`
+#'
+#' A list of [lubridate::interval()] vectors. Each interval vector is merged
+#' into a set of non-overlapping and non-adjacent time spans. Empty (length 0)
+#' interval vectors are returned as an empty set of time spans (i.e. a `<hole>`).
+#'
+#' @param tzone `[character(1)]`
+#'
+#' A recognized timezone to display the `<phinterval>` in. If `tzone` is `NULL`
+#' (the default), then the timezone of the first element of `intervals` is used.
+#'
+#' @return
+#'
+#' A `<phinterval>` vector.
+#'
+#' @examples
+#' jan <- interval(as.Date("2000-01-01"), as.Date("2000-02-01"), tz = "UTC")
+#' feb <- interval(as.Date("2000-02-01"), as.Date("2000-03-01"), tz = "UTC")
+#' nov <- interval(as.Date("2000-11-01"), as.Date("2000-12-01"), tz = "UTC")
+#'
+#' phinterval(list(jan, c(feb, nov)))
+#' phinterval(list(jan), tzone = "EST")
+#' phinterval()
+#'
+#' # Empty (length 0) intervals can be used to create <hole> elements
+#' phinterval(list(interval()))
+#'
+#' # Abutting or overlapping intervals are merged into one span
+#' phinterval(list(c(jan, feb)))
+#'
 #' @export
 phinterval <- function(intervals = list(), tzone = NULL) {
   if (!is_bare_list(intervals)) {
@@ -113,26 +160,86 @@ vec_cast.phinterval.phinterval <- function(x, to, ...) x
 #' @export
 vec_cast.phinterval.Interval <- function(x, to, ...) as_phinterval(x)
 
+#' Test if the object is a phinterval
+#'
+#' @description
+#'
+#' This function returns `TRUE` for `<phinterval>` vectors and returns
+#' `FALSE` otherwise.
+#'
+#' @param x An object to test.
+#' @return `TRUE` if `x` is a `<phinterval>`, `FALSE` otherwise.
+#'
+#' @examples
+#' is_phinterval(phinterval())
+#' is_phinterval(interval())
+#'
 #' @export
 is_phinterval <- function(x) {
   inherits(x, "phinterval")
 }
 
+#' Test if the object is a phinterval or interval
+#'
+#' @description
+#'
+#' This function returns `TRUE` for `<phinterval>` and `<Interval>` vectors and
+#' returns `FALSE` otherwise.
+#'
+#' @param x An object to test.
+#' @return `TRUE` if `x` is a `<phinterval>` or `<Interval>`, `FALSE` otherwise.
+#'
+#' @examples
+#' is_phinterval(phinterval())
+#' is_phinterval(interval())
+#' is_phinterval(as.Date("2020-01-01"))
+#'
 #' @export
 is_phintish <- function(x) {
   is_phinterval(x) || lubridate::is.interval(x)
 }
 
+#' Convert an interval vector into a phinterval
+#'
+#' @description
+#'
+#' `as_phinterval()` changes a [lubridate::interval()] vector into the equivalent
+#' `<phinterval>` vector. Negative intervals are flipped to positive (i.e.
+#' via [lubridate::int_standardize()]).
+#'
+#' @param x An object to convert.
+#' @param ... Parameters passed to other methods. Currently unused.
+#' @return A `<phinterval>` vector the same length as `x`.
+#'
+#' @seealso [phinterval()]
+#'
+#' @examples
+#' years <- interval(
+#'   start = as.Date(c("2021-01-01", "2023-01-01")),
+#'   end =  as.Date(c("2022-01-01", "2024-01-01"))
+#' )
+#' as_phinterval(years)
+#'
+#' # Negative intervals are standardized
+#' (negative <- interval(as.Date("2000-10-11"), as.Date("2000-10-11") - 10))
+#' as_phinterval(negative)
+#'
+#' # A <phinterval> cannot have partially missing endpoints
+#' (partial_na <- interval(NA, as.Date("1999-08-02")))
+#' as_phinterval(partial_na)
+#'
 #' @export
 as_phinterval <- function(x, ...) {
   UseMethod("as_phinterval")
 }
 
+#' @rdname as_phinterval
 #' @export
 as_phinterval.default <- function(x, ...) {
   vec_cast(x, new_phinterval())
 }
 
+#' @rdname as_phinterval
 #' @export
 as_phinterval.Interval <- function(x, ...) {
   new_phinterval(
@@ -165,16 +272,38 @@ vec_arith.phinterval.Duration <- function(op, x, y, ...) {
 
 # accessors --------------------------------------------------------------------
 
+#' Count the number of spans in a phinterval
+#'
+#' @description
+#'
+#' `n_spans()` counts the number of disjoint time spans in each element of
+#' `phint`.
+#'
+#' @inheritParams params
+#' @return An integer vector the same length as `phint`.
+#'
+#' @examples
+#' y2000 <- interval(as.Date("2000-01-01"), as.Date("2001-01-01"))
+#' y2025 <- interval(as.Date("2025-01-01"), as.Date("2025-01-01"))
+#'
+#' n_spans(c(
+#'  phint_union(y2000, y2025),
+#'  phint_intersect(y2000, y2025),
+#'  y2000, 2025
+#' ))
+#'
 #' @export
 n_spans <- function(phint) {
   UseMethod("n_spans")
 }
 
+#' @rdname n_spans
 #' @export
 n_spans.default <- function(phint) {
   check_is_phintish(phint)
 }
 
+#' @rdname n_spans
 #' @export
 n_spans.Interval <- function(phint) {
   out <- rep(1L, length(phint))
@@ -182,6 +311,7 @@ n_spans.Interval <- function(phint) {
   out
 }
 
+#' @rdname n_spans
 #' @export
 n_spans.phinterval <- function(phint) {
   out <- rep(NA_integer_, length(phint))
@@ -190,21 +320,91 @@ n_spans.phinterval <- function(phint) {
   out
 }
 
+#' Test for empty intervals
+#'
+#' @description
+#'
+#' `is_hole()` checks for `<hole>` (i.e. empty) time spans in `phint`.
+#'
+#' @inheritParams params
+#' @return A logical vector the same length as `phint`.
+#'
+#' @examples
+#' y2000 <- interval(as.Date("2000-01-01"), as.Date("2001-01-01"))
+#' y2025 <- interval(as.Date("2025-01-01"), as.Date("2025-01-01"))
+#'
+#' # The intersection of disjoint intervals is a hole
+#' is_hole(c(
+#'  phint_intersect(y2000, y2025),
+#'  y2000, 2025
+#' ))
+#'
 #' @export
 is_hole <- function(phint) {
   n_spans(phint) == 0L
 }
 
+#' Accessors for the endpoints of a phinterval
+#'
+#' @description
+#'
+#' `phint_start()` and `phint_end()` return the earliest and latest endpoint
+#' of a phinterval respectively. Empty (i.e. `<hole>`) time spans are coerced
+#' to `NA` datetimes.
+#'
+#' `phint_starts()` and `phint_ends()` return a list of starts and ends of a
+#' phinterval respectively. Empty time spans are returned as length 0
+#' `<POSIXct>` elements.
+#'
+#' @inheritParams params
+#'
+#' @return
+#'
+#' For `phint_start()` and `phint_end()`, a `<POSIXct>` vector the same length
+#' as `phint`.
+#'
+#' For `phint_start()` and `phint_ends()`, a list of `<POSIXct>` vectors the
+#' same length as `phint`.
+#'
+#' @name phinterval-accessors
+#'
+#' @examples
+#' int1 <- interval(as.Date("2020-01-10"), as.Date("2020-02-01"))
+#' int2 <- interval(as.Date("2023-05-02"), as.Date("2023-06-03"))
+#'
+#' phint_start(int1)
+#' phint_end(int1)
+#'
+#' # Empty <hole> times spans have no start or end date. Time spans containing
+#' # gaps have multiple starts and ends.
+#' hole <- phint_intersect(int1, int2)
+#' disjoint <- phint_union(int1, int2)
+#'
+#' phint_start(c(hole, disjoint))
+#' phint_starts(c(hole, disjoint))
+#'
+#' phint_end(c(hole, disjoint))
+#' phint_ends(c(hole, disjoint))
+#'
+#' # phint_start() and phint_end() return the minimum and maximum endpoints
+#' negative <- interval(as.Date("1980-01-01"), as.Date("1980-01-01") - 5)
+#' phint_start(negative)
+#' phint_end(negative)
+NULL
+
+#' @rdname phinterval-accessors
 #' @export
 phint_start <- function(phint) {
   UseMethod("phint_start")
 }
 
+#' @rdname phinterval-accessors
 #' @export
 phint_start.default <- function(phint) {
   check_is_phintish(phint)
 }
 
+#' @rdname phinterval-accessors
 #' @export
 phint_start.Interval <- function(phint) {
   out <- lubridate::int_start(lubridate::int_standardize(phint))
@@ -212,22 +412,26 @@ phint_start.Interval <- function(phint) {
   out
 }
 
+#' @rdname phinterval-accessors
 #' @export
 phint_start.phinterval <- function(phint) {
   origin <- lubridate::with_tz(lubridate::origin, tzone = get_tzone(phint))
   cpp_interval_sets_start(vec_data(phint)) + origin
 }
 
+#' @rdname phinterval-accessors
 #' @export
 phint_end <- function(phint) {
   UseMethod("phint_end")
 }
 
+#' @rdname phinterval-accessors
 #' @export
 phint_end.default <- function(phint) {
   check_is_phintish(phint)
 }
 
+#' @rdname phinterval-accessors
 #' @export
 phint_end.Interval <- function(phint) {
   out <- lubridate::int_end(lubridate::int_standardize(phint))
@@ -235,89 +439,137 @@ phint_end.Interval <- function(phint) {
   out
 }
 
+#' @rdname phinterval-accessors
 #' @export
 phint_end.phinterval <- function(phint) {
   origin <- lubridate::with_tz(lubridate::origin, tzone = get_tzone(phint))
   cpp_interval_sets_end(vec_data(phint)) + origin
 }
 
+#' @rdname phinterval-accessors
 #' @export
 phint_starts <- function(phint) {
   UseMethod("phint_starts")
 }
 
+#' @rdname phinterval-accessors
 #' @export
 phint_starts.default <- function(phint) {
   check_is_phintish(phint)
 }
 
+#' @rdname phinterval-accessors
 #' @export
 phint_starts.Interval <- function(phint) {
   as.list(phint_start(phint))
 }
 
+#' @rdname phinterval-accessors
 #' @export
 phint_starts.phinterval <- function(phint) {
   origin <- lubridate::with_tz(lubridate::origin, tzone = get_tzone(phint))
   map(cpp_interval_sets_starts(vec_data(phint)), `+`, origin)
 }
 
+#' @rdname phinterval-accessors
 #' @export
 phint_ends <- function(phint) {
   UseMethod("phint_ends")
 }
 
+#' @rdname phinterval-accessors
 #' @export
 phint_ends.default <- function(phint) {
   check_is_phintish(phint)
 }
 
+#' @rdname phinterval-accessors
 #' @export
 phint_ends.Interval <- function(phint) {
   as.list(phint_end(phint))
 }
 
+#' @rdname phinterval-accessors
 #' @export
 phint_ends.phinterval <- function(phint) {
   origin <- lubridate::with_tz(lubridate::origin, tzone = get_tzone(phint))
   map(cpp_interval_sets_ends(vec_data(phint)), `+`, origin)
 }
 
+#' Compute the length of a phinterval in seconds
+#'
+#' @details
+#'
+#' `phint_length()` calculates the total length of time spans within a
+#' phinterval. Instantaneous and empty time spans are 0 seconds long.
+#'
+#' `phint_lengths()` calculates the length in seconds of each time span
+#' in a phinterval.
+#'
+#' @inheritParams params
+#'
+#' @return
+#'
+#' For `phint_length()`, a numeric vector the same length as `phint`.
+#'
+#' For `phint_lengths()`, a list of numeric vectors the same length as `phint`.
+#'
+#' @examples
+#' monday <- interval(as.Date("2025-11-10"), as.Date("2025-11-11"))
+#' friday <- interval(as.Date("2025-11-14"), as.Date("2025-11-15"))
+#'
+#' phint_length(monday)
+#' phint_length(phint_intersect(monday, friday))
+#'
+#' # phint_length() sums the lengths of disjoint time spans
+#' mon_and_fri <- phint_union(monday, friday)
+#' phint_length(mon_and_fri) == phint_length(monday) + phint_length(friday)
+#'
+#' # phint_lengths() returns the length of each disjoint time span
+#' phint_lengths(mon_and_fri)
+#'
 #' @export
 phint_length <- function(phint) {
   UseMethod("phint_length")
 }
 
+#' @rdname phint_length
 #' @export
 phint_length.default <- function(phint) {
   check_is_phintish(phint)
 }
 
+#' @rdname phint_length
 #' @export
 phint_length.Interval <- function(phint) {
   lubridate::int_length(phint)
 }
 
+#' @rdname phint_length
 #' @export
 phint_length.phinterval <- function(phint) {
   map_dbl(phint_lengths(phint), sum)
 }
 
+#' @rdname phint_length
 #' @export
 phint_lengths <- function(phint) {
   UseMethod("phint_lengths")
 }
 
+#' @rdname phint_length
 #' @export
 phint_lengths.default <- function(phint) {
   check_is_phintish(phint)
 }
 
+#' @rdname phint_length
 #' @export
 phint_lengths.Interval <- function(phint) {
   as.list(lubridate::int_length(phint))
 }
 
+#' @rdname phint_length
 #' @export
 phint_lengths.phinterval <- function(phint) {
   data <- vec_data(phint)
@@ -326,22 +578,74 @@ phint_lengths.phinterval <- function(phint) {
     cpp_interval_sets_starts(data),
     `-`
   )
-  out[is_hole(phint)] <- 0
+  out[is_hole(phint)] <- list(0)
   out
 }
 
 # miscellaneous ----------------------------------------------------------------
 
+#' Get the gaps in a phinterval as time spans
+#'
+#' @description
+#'
+#' `phint_invert()` returns the gaps in a phinterval as a `<phinterval>` vector.
+#' Contiguous time spans (e.g. [lubridate::interval()] vectors) are inverted to
+#' `<hole>` time spans.
+#'
+#' `phint_invert()` is similar to `phint_complement()`, except that the time occurring
+#' outside the extent of `phint` (i.e. before its earliest start or after its
+#' latest end) is not included in the result.
+#'
+#' @inheritParams params
+#'
+#' @param hole_to `["hole" / "inf" / "na"]`
+#'
+#' What to turn `<hole>` (i.e. empty) time spans into.
+#' - If `hole_to = "hole"` (the default), `<hole>` spans remain as `<hole>` elements.
+#' - If `"inf"`, they are returned as a time span from `-Inf` to `Inf`.
+#' - If `"na"`, they are returned as a missing (`NA`) span.
+#'
+#' @return
+#'
+#' A `<phinterval>` vector the same length as `phint`.
+#'
+#' @examples
+#' monday <- interval(as.Date("2025-11-10"), as.Date("2025-11-11"))
+#' friday <- interval(as.Date("2025-11-14"), as.Date("2025-11-15"))
+#' sunday <- interval(as.Date("2025-11-16"), as.Date("2025-11-17"))
+#'
+#' # Contiguous intervals are inverted to holes,
+#' # disjoint intervals to spans
+#' phint_invert(monday)
+#' phint_invert(phint_squash(c(monday, friday, sunday))
+#'
+#' tues_to_thurs <- interval(as.Date("2025-11-11"), as.Date("2025-11-14"))
+#' phint_invert(phint_union(monday, friday)) == tues_to_thurs
+#'
+#' # The time before `monday` and after `friday` is included
+#' # in the complement, but not the inversion
+#' mon_and_fri <- phint_union(monday, friday)
+#' phint_invert(mon_and_fri)
+#' phint_complement(mon_and_fri)
+#'
+#' # Specify how to invert empty time spans
+#' hole <- phint_intersect(monday, friday)
+#' phint_invert(hole, hole_to = "hole")
+#' phint_invert(hole, hole_to = "inf")
+#' phint_invert(hole, hole_to = "na")
+#'
 #' @export
 phint_invert <- function(phint, hole_to = c("hole", "inf", "na")) {
   UseMethod("phint_invert")
 }
 
+#' @rdname phint_invert
 #' @export
 phint_invert.default <- function(phint, hole_to = c("hole", "inf", "na")) {
   check_is_phintish(phint)
 }
 
+#' @rdname phint_invert
 #' @export
 phint_invert.Interval <- function(phint, hole_to = c("hole", "inf", "na")) {
   arg_match(hole_to)
@@ -350,6 +654,7 @@ phint_invert.Interval <- function(phint, hole_to = c("hole", "inf", "na")) {
   new_phinterval(interval_sets, tzone = get_tzone(phint))
 }
 
+#' @rdname phint_invert
 #' @export
 phint_invert.phinterval <- function(phint, hole_to = c("hole", "inf", "na")) {
   hole_to <- arg_match(hole_to)
@@ -362,6 +667,42 @@ phint_invert.phinterval <- function(phint, hole_to = c("hole", "inf", "na")) {
   new_phinterval(interval_sets = interval_sets, tzone = get_tzone(phint))
 }
 
+#' Convert a phinterval into a list of intervals
+#'
+#' @description
+#'
+#' `phint_to_spans()` decomposes each element of a phinterval into the set
+#' of its contiguous time spans, returned as [lubridate::interval()] vectors.
+#'
+#' @inheritParams params
+#'
+#' @param hole_to `["empty" / "na" / "null"]`
+#'
+#' What to turn `<hole>` (i.e. empty) time spans into.
+#' If `hole_to = "empty"` (the default), `<hole>` spans are returned as length 0 intervals.
+#' If `"na"`, they are returned as a missing (`NA`) interval.
+#' If `"null"`, they are returned as a `NULL` element.
+#'
+#' @return
+#'
+#' A list of `<Interval>` vectors the same length as `phint`. If `hole_to = "null"`,
+#' the list may contain `NULL` elements.
+#'
+#' @examples
+#' monday <- interval(as.Date("2025-11-10"), as.Date("2025-11-11"))
+#' friday <- interval(as.Date("2025-11-14"), as.Date("2025-11-15"))
+#' sunday <- interval(as.Date("2025-11-16"), as.Date("2025-11-17"))
+#'
+#' mon_and_fri <- phint_union(monday, friday)
+#' phint_to_spans(mon_and_fri)
+#' phint_to_spans(sunday)
+#'
+#' # Specify how to invert empty time spans
+#' hole <- phint_intersect(monday, friday)
+#' phint_to_spans(hole, hole_to = "empty")
+#' phint_to_spans(hole, hole_to = "na")
+#' phint_to_spans(hole, hole_to = "NULL")
+#'
 #' @export
 phint_to_spans <- function(phint, hole_to = c("empty", "na", "null")) {
   hole_to <- arg_match(hole_to)
@@ -374,6 +715,29 @@ phint_to_spans <- function(phint, hole_to = c("empty", "na", "null")) {
   out
 }
 
+#' Remove instantaneous time spans from a phinterval
+#'
+#' @description
+#'
+#' `phint_sift()` removes instants (i.e. spans of 0 seconds in duration) from
+#' elements of a phinterval.
+#'
+#' @inheritParams params
+#' @return A `<phinterval>` vector the same length as `phint`.
+#'
+#' @examples
+#' y2020 <- interval(as.Date("2020-01-01"), as.Date("2021-01-01"))
+#' y2021 <- interval(as.Date("2021-01-01"), as.Date("2022-01-01"))
+#' y2022 <- interval(as.Date("2022-01-01"), as.Date("2023-01-01"))
+#'
+#' # The intersection of two adjacent intervals is an instant.
+#' # phint_sift() is useful for removing these instants.
+#' (new_years_2021 <- phint_intersect(y2020, y2021))
+#' phint_sift(new_years_2021)
+#'
+#' (y2022_and_new_years_2021 <- phint_union(y2022, new_years_2021))
+#' phint_sift(y2022_and_new_years_2021)
+#'
 #' @export
 phint_sift <- function(phint) {
   check_is_phintish(phint)
@@ -383,17 +747,41 @@ phint_sift <- function(phint) {
   )
 }
 
+#' Convert a phinterval to a duration
+#'
+#' @description
+#'
+#' `as_duration()` changes [lubridate::interval()] and `<phinterval>` vectors
+#' into [lubridate::duration()] vectors. The resulting duration measures the
+#' length of time in seconds within each element of the interval or phinterval.
+#'
+#' `as_duration()` is a wrapper around [lubridate::as.duration()].
+#'
+#' @param x An object to convert.
+#' @param ... Parameters passed to other methods. Currently unused.
+#' @return A `<Duration>` vector the same length as `x`.
+#'
+#' @examples
+#' monday <- interval(as.Date("2025-11-10"), as.Date("2025-11-11"))
+#' friday <- interval(as.Date("2025-11-14"), as.Date("2025-11-15"))
+#' mon_and_fri <- phint_union(monday, friday)
+#'
+#' as_duration(c(mon_and_fri, monday))
+#' as_duration(mon_and_fri) == as_duration(monday) + as_duration(friday)
+#'
 #' @export
-as_duration <- function(x) {
+as_duration <- function(x, ...) {
   UseMethod("as_duration")
 }
 
+#' @rdname as_duration
 #' @export
-as_duration.default <- function(x) {
+as_duration.default <- function(x, ...) {
   lubridate::as.duration(x)
 }
 
+#' @rdname as_duration
 #' @export
-as_duration.phinterval <- function(x) {
+as_duration.phinterval <- function(x, ...) {
   lubridate::as.duration(phint_length(x))
 }
--- R/set-operations.R
+++ R/set-operations.R
@@ -1,5 +1,57 @@
-# complement -------------------------------------------------------------------
+#' Vectorized set operations
+#'
+#' @description
+#'
+#' These functions perform elementwise set operations on `<phinterval>` vectors,
+#' treating each element as a set of non-overlapping intervals. They return a new
+#' `<phinterval>` vector representing the result of the corresponding set
+#' operation. All functions follow vctrs-style recycling rules.
+#'
+#' - `phint_complement()` returns all time spans *not covered* by `phint`.
+#' - `phint_union()` returns the intervals that are within either `phint1` or `phint2`.
+#' - `phint_intersect()` returns the intervals that are within both `phint1` and `phint2`.
+#' - `phint_setdiff()` returns intervals in `phint1` that are not within `phint2`.
+#'
+#' @inheritParams params
+#' @return A `<phinterval>` vector.
+#'
+#' @examples
+#' monday  <- interval(as.Date("2025-11-10"), as.Date("2025-11-11"))
+#' tuesday <- interval(as.Date("2025-11-11"), as.Date("2025-11-12"))
+#' friday  <- interval(as.Date("2025-11-14"), as.Date("2025-11-15"))
+#' jan_1_to_5 <- interval(as.Date("2000-01-01"), as.Date("2000-01-05"))
+#' jan_3_to_9 <- interval(as.Date("2000-01-03"), as.Date("2000-01-09"))
+#'
+#' phint_complement(jan_1_to_5)
+#'
+#' # The complement of a <hole> is an infinite span covering all dates
+#' hole <- phinterval(interval())
+#' phint_complement(hole)
+#'
+#' phint_union(c(monday, monday, monday), c(tuesday, friday, NA))
+#'
+#' # Elements of length 1 are recycled
+#' phint_union(monday, c(tuesday, friday, NA))
+#'
+#' phint_intersect(jan_1_to_5, jan_3_to_9)
+#'
+#' # The intersection of non-overlapping intervals is a <hole>
+#' phint_intersect(monday, friday)
+#'
+#' # The intersection of adjacent intervals is an instant
+#' phint_intersect(monday, tuesday)
+#'
+#' phint_setdiff(jan_1_to_5, jan_3_to_9)
+#' phint_setdiff(jan_3_to_9, jan_1_to_5)
+#'
+#' # Instantaneous intervals do not affect the set difference
+#' noon_monday <- as.POSIXct("2025-11-10 12:00:00")
+#' phint_setdiff(monday, interval(noon_monday, noon_monday)) == monday
+#'
+#' @name phinterval-set-operations
+NULL
 
+#' @rdname phinterval-set-operations
 #' @export
 phint_complement <- function(phint) {
   check_is_phintish(phint)
@@ -9,8 +61,7 @@ phint_complement <- function(phint) {
   )
 }
 
-# union ------------------------------------------------------------------------
-
+#' @rdname phinterval-set-operations
 #' @export
 phint_union <- function(phint1, phint2) {
   phints <- validate_phints(phint1, phint2)
@@ -20,8 +71,7 @@ phint_union <- function(phint1, phint2) {
   )
 }
 
-# intersection -----------------------------------------------------------------
-
+#' @rdname phinterval-set-operations
 #' @export
 phint_intersect <- function(phint1, phint2) {
   phints <- validate_phints(phint1, phint2)
@@ -31,8 +81,7 @@ phint_intersect <- function(phint1, phint2) {
   )
 }
 
-# difference -------------------------------------------------------------------
-
+#' @rdname phinterval-set-operations
 #' @export
 phint_setdiff <- function(phint1, phint2) {
   phints <- validate_phints(phint1, phint2)
@@ -44,6 +93,56 @@ phint_setdiff <- function(phint1, phint2) {
 
 # squash -----------------------------------------------------------------------
 
+#' Flatten a phinterval
+#'
+#' @description
+#'
+#' `phint_squash()` combines all elements of a `<phinterval>` vector into a
+#' single set of non-overlapping, non-adjacent time spans, returned as a
+#' scalar `<phinterval>` by default.
+#'
+#' This is useful in conjunction with aggregation functions, e.g.
+#' [dplyr::summarize()]).
+#'
+#' @inheritParams params
+#'
+#' @param na.rm `[TRUE / FALSE]`
+#'
+#' Whether to ignore `NA` elements when flattening a `<phinterval>`.
+#'
+#' If `na.rm = FALSE` and `phint` contains any `NA` elements, then a scalar `NA`
+#' `<phinterval>` will be returned.
+#'
+#' @param empty_to
+#'
+#' What to return if the input `phint` is length 0.
+#' - If `empty_to = "na"` (the default), a length 1 `NA` `<phinterval>` is returned.
+#' - If `"hole"` a single `<hole>` is returned.
+#' - If `"empty"` a length 0 `<phinterval>` vector is returned.
+#'
+#' @return
+#'
+#' A length 1 `<phinterval>` vector, with one exception.
+#'
+#' If the input is an empty `<phinterval>` or `<Interval>` vector *and*
+#' `empty_to = "empty"`, then the result is a length 0 `<phinterval>` vector.
+#'
+#' @examples
+#' jan_1_to_5   <- interval(as.Date("2000-01-01"), as.Date("2000-01-05"))
+#' jan_3_to_9   <- interval(as.Date("2000-01-03"), as.Date("2000-01-09"))
+#' jan_11_to_12 <- interval(as.Date("2000-01-11"), as.Date("2000-01-12"))
+#'
+#' phint_squash(c(jan_1_to_5, jan_3_to_9, jan_11_to_12, NA))
+#'
+#' # Set `na.rm = FALSE` to propagate `NA` values
+#' phint_squash(c(jan_1_to_5, jan_3_to_9, jan_11_to_12, NA), na.rm = FALSE)
+#'
+#' # `empty_to` determines the result of empty inputs
+#' empty <- interval()
+#' phint_squash(empty, empty_to = "na")
+#' phint_squash(empty, empty_to = "hole")
+#' phint_squash(empty, empty_to = "empty")
+#'
 #' @export
 phint_squash <- function(phint, na.rm = TRUE, empty_to = c("na", "hole", "empty")) {
   UseMethod("phint_squash")
@@ -114,6 +213,33 @@ empty_result <- function(empty_to) {
 
 # overlaps ---------------------------------------------------------------------
 
+#' Test whether two phintervals overlap
+#'
+#' @description
+#'
+#' `phint_overlaps()` tests whether the i-th element of `phint1` overlaps
+#' with the i-th element of `phint2`, returning a logical vector. Adjacent
+#' intervals are considered overlapping. `phint1` and `phint2` are recycled to
+#' their common length using vctrs-style recycling rules.
+#'
+#' @inheritParams params
+#' @return A logical vector.
+#'
+#' @examples
+#' monday <- interval(as.Date("2025-11-10"), as.Date("2025-11-11"))
+#' tuesday <- interval(as.Date("2025-11-11"), as.Date("2025-11-12"))
+#' friday <- interval(as.Date("2025-11-14"), as.Date("2025-11-15"))
+#' mon_and_fri <- phint_union(monday, friday)
+#'
+#' phint_overlaps(c(monday, monday, friday), c(friday, mon_and_fri, NA))
+#'
+#' # Adjacent intervals are considered overlapping
+#' phint_overlaps(monday, tuesday)
+#'
+#' # Holes are always considered non-overlapping
+#' hole <- phinterval(interval())
+#' phint_overlaps(c(hole, monday), c(hole, hole))
+#'
 #' @export
 phint_overlaps <- function(phint1, phint2) {
   phints <- validate_phints(phint1, phint2)
@@ -122,6 +248,48 @@ phint_overlaps <- function(phint1, phint2) {
 
 # within -----------------------------------------------------------------------
 
+#' Test whether a date, time, or phinterval is within another phinterval
+#'
+#' @description
+#'
+#' `phint_within()` tests whether the i-th element of `x` is contained within
+#' the i-th element of `phint`, returning a logical vector. `x` may be a date or
+#' time, while `phint` must be a phinterval. `x` and `phint` are recycled to
+#' their common length using vctrs-style recycling rules.
+#'
+#' Dates and times on an endpoint of an interval are considered to be within
+#' the interval. An interval is considered to be within itself.
+#'
+#' @inheritParams params
+#'
+#' @param x
+#'
+#' A `<POSIXct>`, `<POSIXlt>`, `<Date>`, `<Interval>` or `<phinterval>`
+#' vector to test.
+#'
+#' @return A logical vector.
+#'
+#' @examples
+#' jan_1_to_5 <- interval(as.Date("2000-01-01"), as.Date("2000-01-05"))
+#' jan_2_to_4 <- interval(as.Date("2000-01-03"), as.Date("2000-01-09"))
+#' jan_3_to_9 <- interval(as.Date("2000-01-03"), as.Date("2000-01-09"))
+#'
+#' phint_within(
+#'   c(jan_2_to_4, jan_3_to_9, jan_1_to_5),
+#'   c(jan_1_to_5, jan_1_to_5, NA)
+#' )
+#'
+#' phint_within(as.Date("2000-01-06", "2000-01-20"), jan_3_to_9)
+#'
+#' # Intervals are within themselves
+#' phint_within(jan_1_to_5, jan_1_to_5)
+#'
+#' # Interval endpoints are considered to be within the interval
+#' phint_within(as.Date("2000-01-01"), jan_1_to_5)
+#'
+#' # Holes are never considered to be within an interval
+#' phint_within(c(hole, hole), c(hole, jan_1_to_5))
+#'
 #' @export
 phint_within <- function(x, phint) {
   if (lubridate::is.instant(x)) {
@@ -130,7 +298,7 @@ phint_within <- function(x, phint) {
     objs <- vctrs::vec_recycle_common(phint, x)
     cpp_interval_sets_contains(
       phint_data(objs[[1]]),
-      as.numeric(as.POSIXct(objs[[2]]))
+      as.numeric(lubridate::as_datetime(objs[[2]]))
     )
   } else {
     phints <- validate_phints(x, phint)
