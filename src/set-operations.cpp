3dfb1b216ab85cc928a0fb2f5dacf5a8a9268abb
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2026-01-19 17:50:39 -0500

    Inline intersect dispatch instead of using macros

--- src/set-operations.cpp
+++ src/set-operations.cpp
@@ -5,45 +5,58 @@
 #include <Rcpp.h>
 using namespace Rcpp;
 
-#define PHINT_X_ARGS IntegerVector x_size, List x_starts, List x_ends
-#define PHINT_Y_ARGS IntegerVector y_size, List y_starts, List y_ends
-#define INTVL_X_ARGS NumericVector x_starts, NumericVector x_ends
-#define INTVL_Y_ARGS NumericVector y_starts, NumericVector y_ends
-
-#define PHINT_X_INIT const PhintVector x { x_size, x_starts, x_ends }
-#define PHINT_Y_INIT const PhintVector y { y_size, y_starts, y_ends }
-#define INTVL_X_INIT const IntvlVector x { x_starts, x_ends }
-#define INTVL_Y_INIT const IntvlVector y { y_starts, y_ends }
-
-// All binary operations are generated for every combination of
-// PhintVector and IntvlVector.
-#define PHINT_BINARY_OP(f_name, x_args, y_args, x_init, y_init, op) \
-  List f_name(x_args, y_args) {                                     \
-      x_init;                                                       \
-      y_init;                                                       \
-      return phint_operate(x, y, op{});                             \
-  }                                                                 \
+// intersect -------------------------------------------------------------------
 
 // [[Rcpp::export]]
-PHINT_BINARY_OP(
-  phint_phint_intersect_cpp,
-  PHINT_X_ARGS, PHINT_Y_ARGS, PHINT_X_INIT, PHINT_Y_INIT, Intersect
-)
+List phint_phint_intersect_cpp(
+    IntegerVector x_size, List x_starts, List x_ends,
+    IntegerVector y_size, List y_starts, List y_ends
+) {
+  return phint_operate(
+    PhintVector{x_size, x_starts, x_ends},
+    PhintVector{y_size, y_starts, y_ends},
+    Intersect{}
+  );
+}
 
 // [[Rcpp::export]]
-PHINT_BINARY_OP(
-  phint_intvl_intersect_cpp,
-  PHINT_X_ARGS, INTVL_Y_ARGS, PHINT_X_INIT, INTVL_Y_INIT, Intersect
-)
+List phint_intvl_intersect_cpp(
+    IntegerVector x_size, List x_starts, List x_ends,
+    NumericVector y_starts, NumericVector y_ends
+) {
+  return phint_operate(
+    PhintVector{x_size, x_starts, x_ends},
+    IntvlVector{y_starts, y_ends},
+    Intersect{}
+  );
+}
 
 // [[Rcpp::export]]
-PHINT_BINARY_OP(
-  intvl_phint_intersect_cpp,
-  INTVL_X_ARGS, PHINT_Y_ARGS, INTVL_X_INIT, PHINT_Y_INIT, Intersect
-)
+List intvl_phint_intersect_cpp(
+    NumericVector x_starts, NumericVector x_ends,
+    IntegerVector y_size, List y_starts, List y_ends
+) {
+  return phint_operate(
+    IntvlVector{x_starts, x_ends},
+    PhintVector{y_size, y_starts, y_ends},
+    Intersect{}
+  );
+}
 
 // [[Rcpp::export]]
-PHINT_BINARY_OP(
-  intvl_intvl_intersect_cpp,
-  INTVL_X_ARGS, INTVL_Y_ARGS, INTVL_X_INIT, INTVL_Y_INIT, Intersect
-)
+List intvl_intvl_intersect_cpp(
+    NumericVector x_starts, NumericVector x_ends,
+    NumericVector y_starts, NumericVector y_ends
+) {
+  return phint_operate(
+    IntvlVector{x_starts, x_ends},
+    IntvlVector{y_starts, y_ends},
+    Intersect{}
+  );
+}
+
+// union -----------------------------------------------------------------------
+
+// setdiff ---------------------------------------------------------------------
+
+// symmetric setdiff -----------------------------------------------------------
