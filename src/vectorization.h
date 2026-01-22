61b2fbfac1086be907f8273c10bd4153809d44de
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2026-01-22 18:03:28 -0500

    Use constexpr to take different paths for span types (<Interval>) and set types (<phinterval>)

--- src/vectorization.h
+++ src/vectorization.h
@@ -1,17 +1,18 @@
 #ifndef PHINTERVAL_VECTORIZATION_H
 #define PHINTERVAL_VECTORIZATION_H
 
+#include "type-helpers.h"
 #include "type-interval.h"
 #include "type-phinterval.h"
 #include <algorithm>
 #include <Rcpp.h>
 using namespace Rcpp;
 
-template <typename XVector, typename YVector, typename Op>
-List phint_operate_impl(XVector x, YVector y, Op op, R_xlen_t n);
+template <typename VectorX, typename VectorY, typename Op>
+List phint_operate_impl(VectorX x, VectorY y, Op op, R_xlen_t n);
 
-template <typename XVector, typename YVector, typename Op>
-List phint_operate(const XVector& x, const YVector& y, Op op) {
+template <typename VectorX, typename VectorY, typename Op>
+List phint_operate(const VectorX& x, const VectorY& y, Op op) {
   const R_xlen_t x_n = x.n_sets();
   const R_xlen_t y_n = y.n_sets();
   const R_xlen_t n = std::max(x_n, y_n);
@@ -22,8 +23,8 @@ List phint_operate(const XVector& x, const YVector& y, Op op) {
   return phint_operate_impl(x, y, op, n);
 }
 
-template <typename XVector, typename YVector, typename Op>
-List phint_operate_impl(XVector x, YVector y, Op op, R_xlen_t n) {
+template <typename VectorX, typename VectorY, typename Op>
+List phint_operate_impl(VectorX x, VectorY y, Op op, R_xlen_t n) {
   PhintBuffer buffer(n);
 
   for (R_xlen_t i = 0; i < n; i++) {
@@ -34,10 +35,20 @@ List phint_operate_impl(XVector x, YVector y, Op op, R_xlen_t n) {
 
     if (x_view.is_na || y_view.is_na) {
       buffer.add_na_element();
-    } else if (x_view.is_scalar() && y_view.is_scalar()) {
+      continue;
+    }
+
+    // Requires C++17, skips scalar check for span vectors (e.g. IntvlVector)
+    if constexpr (is_scalar_view<decltype(x_view)> && is_scalar_view<decltype(y_view)>) {
+      // Both vectors are scalar types (e.g. not a PhintVector)
       op.apply_to_span(x_view, y_view, buffer);
     } else {
-      op.apply_to_set(x_view, y_view, buffer);
+      // One of the vectors is a PhintVector, check for scalar set at run-time
+      if (x_view.is_scalar() && y_view.is_scalar()) {
+        op.apply_to_span(x_view, y_view, buffer);
+      } else {
+        op.apply_to_set(x_view, y_view, buffer);
+      }
     }
   }
 
