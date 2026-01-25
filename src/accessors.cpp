498a1f68b01f0a9e22ea648d157d19570c435c83
Ethan Sansom <99099398+EthanSansom@users.noreply.github.com>
2026-01-25 18:15:11 -0500

    Bug fix + refactor: phint_starts/ends_cpp() was inserting NA_real_ instead of NA_POSIXct_ elements

--- src/accessors.cpp
+++ src/accessors.cpp
@@ -5,89 +5,57 @@ using namespace Rcpp;
 
 // scalar start/end ------------------------------------------------------------
 
+template <bool PointIsStart>
+DatetimeVector phint_point_impl(IntegerVector size, List points, String tzone);
+
+template <typename VectorType, bool PointIsStart>
+DatetimeVector scalar_point_impl(const VectorType& vec, String tzone);
+
 // [[Rcpp::export]]
-DatetimeVector phint_start_cpp(IntegerVector size, List starts, List ends, String tzone) {
-  const R_xlen_t n = size.size();
-  const int* p_size = INTEGER(size);
-  NumericVector out = no_init(n);
-  double* p_out = REAL(out);
-
-  for (R_xlen_t i = 0; i < n; i++) {
-    if (!(i & 8191)) checkUserInterrupt();
-
-    int size_i = p_size[i];
-
-    if (size_i == NA_INTEGER || size_i == 0) {
-      p_out[i] = NA_REAL;
-      continue;
-    }
-
-    SEXP starts_i = VECTOR_ELT(starts, i);
-    const double* p_starts_i = REAL(starts_i);
-    p_out[i] = p_starts_i[0];
-  }
-
-  DatetimeVector result(out);
-  result.attr("tzone") = tzone;
-  return result;
+DatetimeVector phint_start_cpp(IntegerVector size, List starts, String tzone) {
+  return phint_point_impl<true>(size, starts, tzone);
 }
 
 // [[Rcpp::export]]
-DatetimeVector phint_end_cpp(IntegerVector size, List starts, List ends, String tzone) {
-  const R_xlen_t n = size.size();
-  const int* p_size = INTEGER(size);
-  NumericVector out = no_init(n);
-  double* p_out = REAL(out);
-
-  for (R_xlen_t i = 0; i < n; i++) {
-    if (!(i & 8191)) checkUserInterrupt();
-
-    int size_i = p_size[i];
-
-    if (size_i == NA_INTEGER || size_i == 0) {
-      p_out[i] = NA_REAL;
-      continue;
-    }
-
-    SEXP ends_i = VECTOR_ELT(ends, i);
-    const double* p_ends_i = REAL(ends_i);
-    p_out[i] = p_ends_i[size_i - 1]; // Adjust for 0-based index
-  }
-
-  DatetimeVector result(out);
-  result.attr("tzone") = tzone;
-  return result;
+DatetimeVector phint_end_cpp(IntegerVector size, List ends, String tzone) {
+  return phint_point_impl<false>(size, ends, tzone);
 }
-
-template <typename VectorType>
-DatetimeVector scalar_start_impl(const VectorType& vec, String tzone);
-
-template <typename VectorType>
-DatetimeVector scalar_end_impl(const VectorType& vec, String tzone);
 
 // [[Rcpp::export]]
 DatetimeVector intvl_start_cpp(DatetimeVector starts, NumericVector spans, String tzone) {
   IntvlVector vec { starts, spans };
-  return scalar_start_impl(vec, tzone);
+  return scalar_point_impl<IntvlVector, true>(vec, tzone);
 }
 
 // [[Rcpp::export]]
 DatetimeVector intvl_end_cpp(DatetimeVector starts, NumericVector spans, String tzone) {
   IntvlVector vec { starts, spans };
-  return scalar_end_impl(vec, tzone);
+  return scalar_point_impl<IntvlVector, false>(vec, tzone);
 }
 
-template <typename VectorType>
-DatetimeVector scalar_start_impl(const VectorType& vec, String tzone) {
-  const R_xlen_t n = vec.n_sets();
+template <bool PointIsStart>
+DatetimeVector phint_point_impl(IntegerVector size, List points, String tzone) {
+  const R_xlen_t n = size.size();
+  const int* p_size = INTEGER(size);
   NumericVector out = no_init(n);
   double* p_out = REAL(out);
 
   for (R_xlen_t i = 0; i < n; i++) {
     if (!(i & 8191)) checkUserInterrupt();
 
-    auto view = vec.view(i);
-    p_out[i] = view.is_na ? NA_REAL : view.start(0);
+    int size_i = p_size[i];
+
+    if (size_i == NA_INTEGER || size_i == 0) {
+      p_out[i] = NA_REAL;
+      continue;
+    }
+
+    const double* p_points_i = REAL(VECTOR_ELT(points, i));
+    if constexpr (PointIsStart) {
+      p_out[i] = p_points_i[0];
+    } else {
+      p_out[i] = p_points_i[size_i - 1]; // Adjust for 0-based index
+    }
   }
 
   DatetimeVector result(out);
@@ -95,8 +63,8 @@ DatetimeVector scalar_start_impl(const VectorType& vec, String tzone) {
   return result;
 }
 
-template <typename VectorType>
-DatetimeVector scalar_end_impl(const VectorType& vec, String tzone) {
+template <typename VectorType, bool PointIsStart>
+DatetimeVector scalar_point_impl(const VectorType& vec, String tzone) {
   const R_xlen_t n = vec.n_sets();
   NumericVector out = no_init(n);
   double* p_out = REAL(out);
@@ -105,7 +73,11 @@ DatetimeVector scalar_end_impl(const VectorType& vec, String tzone) {
     if (!(i & 8191)) checkUserInterrupt();
 
     auto view = vec.view(i);
-    p_out[i] = view.is_na ? NA_REAL : view.end(0);
+    if constexpr (PointIsStart) {
+      p_out[i] = view.is_na ? NA_REAL : view.start(0);
+    } else {
+      p_out[i] = view.is_na ? NA_REAL : view.end(0);
+    }
   }
 
   DatetimeVector result(out);
@@ -116,48 +88,23 @@ DatetimeVector scalar_end_impl(const VectorType& vec, String tzone) {
 // list starts/ends ------------------------------------------------------------
 
 // [[Rcpp::export]]
-List phint_starts_cpp(IntegerVector size, List starts, List ends, String tzone) {
+List phint_points_cpp(IntegerVector size, List points, String tzone) {
   const R_xlen_t n = size.size();
   const int* p_size = INTEGER(size);
   List out = no_init(n);
 
-  for (R_xlen_t i = 0; i < n; i++) {
-    if (!(i & 8191)) checkUserInterrupt();
-
-    int size_i = p_size[i];
-
-    if (size_i == NA_INTEGER) {
-      SET_VECTOR_ELT(out, i, Rf_ScalarReal(NA_REAL));
-      continue;
-    }
-
-    SEXP starts_i = VECTOR_ELT(starts, i);
-    DatetimeVector dt(clone(starts_i));
-    dt.attr("tzone") = tzone;
-    SET_VECTOR_ELT(out, i, dt);
-  }
-
-  return out;
-}
-
-// [[Rcpp::export]]
-List phint_ends_cpp(IntegerVector size, List starts, List ends, String tzone) {
-  const R_xlen_t n = size.size();
-  const int* p_size = INTEGER(size);
-  List out = no_init(n);
+  DatetimeVector dt_na = DatetimeVector::create(NA_REAL);
+  dt_na.attr("tzone") = tzone;
 
   for (R_xlen_t i = 0; i < n; i++) {
     if (!(i & 8191)) checkUserInterrupt();
 
-    int size_i = p_size[i];
-
-    if (size_i == NA_INTEGER) {
-      SET_VECTOR_ELT(out, i, Rf_ScalarReal(NA_REAL));
+    if (p_size[i] == NA_INTEGER) {
+      SET_VECTOR_ELT(out, i, clone(dt_na));
       continue;
     }
 
-    SEXP ends_i = VECTOR_ELT(ends, i);
-    DatetimeVector dt(clone(ends_i));
+    DatetimeVector dt(clone(VECTOR_ELT(points, i)));
     dt.attr("tzone") = tzone;
     SET_VECTOR_ELT(out, i, dt);
   }
